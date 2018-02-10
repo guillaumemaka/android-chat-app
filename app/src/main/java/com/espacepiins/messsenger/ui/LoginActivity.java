c167bb5b4b5718b7be541bd98a1c0dc6c44106b6
Guillaume Maka <guillaume.maka@gmail.com>
2018-02-10 02:16:12 -0500

    Add Firebase Crashlytics

--- app/src/main/java/com/espacepiins/messsenger/ui/LoginActivity.java
+++ app/src/main/java/com/espacepiins/messsenger/ui/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.espacepiins.messsenger;
+package com.espacepiins.messsenger.ui;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
@@ -28,6 +28,8 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.espacepiins.messsenger.R;
 
 import java.util.ArrayList;
 import java.util.List;
