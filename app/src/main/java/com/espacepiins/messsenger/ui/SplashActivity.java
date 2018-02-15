cec2c606c6c46a057bc2fd2eadc08df3579a1bea
Guillaume Maka <guillaume.maka@gmail.com>
2018-02-15 09:56:16 -0500

    Add Splash Sceen

---
+++ app/src/main/java/com/espacepiins/messsenger/ui/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.espacepiins.messsenger.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.espacepiins.messsenger.R;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
