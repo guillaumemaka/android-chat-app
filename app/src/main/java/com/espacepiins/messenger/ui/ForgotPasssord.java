f51cd5ecaede29d6b6c2bc3356bef9e3d13362a1
Raphael Etang-Salé <ets_raphael@outlook.fr>
2018-03-13 17:04:13 -0400

    WIP

---
+++ app/src/main/java/com/espacepiins/messenger/ui/ForgotPasssord.java
@@ -0,0 +1,19 @@
+package com.espacepiins.messenger.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.espacepiins.messsenger.R;
+
+/**
+ * Created by raphaeletang-sale on 18-02-26.
+ */
+
+public class ForgotPasssord extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forgotpassword_fragment_layout);
+    }
+}
